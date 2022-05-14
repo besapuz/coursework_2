5c2f79d59fc6778c16c567f9f42443232e8c57ec
besapuz <aleksandzarinskij18772@gmail.com>
2022-05-14 19:11:37 +0300

    рабочая версия трех страниц

--- utils.py
+++ utils.py
@@ -49,18 +49,17 @@ def get_comments_by_post_id(post_id, c):
 
 def search_for_posts(query):
     """возвращает список постов по ключевому слову"""
-    word = [i.lower() for i in query.split(" ")]
-    if word:
-        list_content = []
-        for c in word:
-            for n in dict_json:
-                if c in n["content"].lower():
-                    list_content.append(n)
-                else:
-                    continue
-        return list_content
-    else:
-        return False
+    list_content = []
+    query = query.lower()
+    count = 0
+    for c in dict_json:
+        if count <= 10:
+            if query in c["content"].lower():
+                list_content.append(c)
+                count += 1
+            else:
+                continue
+    return list_content
 
 
 def get_post_by_pk(pk):
